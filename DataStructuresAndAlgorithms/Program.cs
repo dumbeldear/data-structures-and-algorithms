04eb39ac6a1bfc6ae2b1ff8aa5771a312cf48b66
Mr Kevin Nguyen <kevinnguyen3047@gmail.com>
2025-11-18 19:10:25 +1100

    Added Get method

--- DataStructuresAndAlgorithms/Program.cs
+++ DataStructuresAndAlgorithms/Program.cs
@@ -1,3 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
 var array = new StaticArray<int>(5);
 Console.WriteLine(array.Capacity);
+Console.WriteLine(array.Get(2));
+Console.WriteLine(array.Get(4));
