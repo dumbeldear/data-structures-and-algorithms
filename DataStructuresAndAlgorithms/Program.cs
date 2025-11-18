4163ef45d599cfb99853a94db7c8c1449973a983
Mr Kevin Nguyen <kevinnguyen3047@gmail.com>
2025-11-18 19:36:29 +1100

    Added Set method

--- DataStructuresAndAlgorithms/Program.cs
+++ DataStructuresAndAlgorithms/Program.cs
@@ -1,5 +1,20 @@
 ﻿// See https://aka.ms/new-console-template for more information
-var array = new StaticArray<int>(5);
-Console.WriteLine(array.Capacity);
-Console.WriteLine(array.Get(2));
-Console.WriteLine(array.Get(4));
+// Initializing static array
+var array = new StaticArray<string>(5);
+Console.WriteLine("Array capacity is: " + array.Capacity);
+
+// Set values in array
+array.Set(0, "Toyota");
+array.Set(1, "Honda");
+array.Set(2, "Mercedes");
+array.Set(3, "BMW");
+// array.Set(4, "Ford");
+// Testing Index out of range exception
+// array.Set(5, "Ford");
+
+// Testing array length after setting
+Console.WriteLine("Array length is: " + array.Length);
+
+// Testing get methods
+Console.WriteLine(array.Get(3));
+
