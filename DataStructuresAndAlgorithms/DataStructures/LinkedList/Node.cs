6041d01fb83aef3e9f1ae4188a0377fb15db1345
Mr Kevin Nguyen <kevinnguyen3047@gmail.com>
2025-11-19 21:11:56 +1100

    Addded properties and constructor for Node class

--- DataStructuresAndAlgorithms/DataStructures/LinkedList/Node.cs
+++ DataStructuresAndAlgorithms/DataStructures/LinkedList/Node.cs
@@ -1,6 +1,16 @@
 namespace DataStructuresAndAlgorithms.DataStructures.LinkedList;
 
-public class Node
+/// <summary>
+/// Making Node class internal - only LinkedList class should be able to see / use Nodes
+/// </summary>
+/// <typeparam name="T"></typeparam>
+internal class Node<T>
 {
-    
+    public T Value { get; }
+    public Node<T>? Next { get; set; }
+
+    public Node(T value)
+    {
+        Value = value;
+    }
 }
